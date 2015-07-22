7e1806c30337d97fb5e9cf51fe91e5266887e6fb
dstamen <davidstamen@Davids-MacBook-Pro.local>
2015-07-22 09:14:53 -0400

    added rescan to guest expansion

--- ExtendVMDKandGuest.ps1
+++ ExtendVMDKandGuest.ps1
@@ -12,4 +12,4 @@ $HardDisk = "Hard Disk " + $HardDisk
 $HardDiskSize = Read-Host "Enter the new Hard Disk size in GB (Ex. 50)"
 $VolumeLetter = Read-Host "Enter the volume letter (Ex. c,d,e,f)"
 Get-HardDisk -vm $VM | where {$_.Name -eq $HardDisk} | Set-HardDisk -CapacityGB $HardDiskSize -Confirm:$false
-Invoke-VMScript -vm $VM -ScriptText "echo select vol $VolumeLetter > c:\diskpart.txt && echo extend >> c:\diskpart.txt && diskpart.exe /s c:\diskpart.txt" -ScriptType BAT
+Invoke-VMScript -vm $VM -ScriptText "echo rescan > c:\diskpart.txt && echo select vol $VolumeLetter >> c:\diskpart.txt && echo extend >> c:\diskpart.txt && diskpart.exe /s c:\diskpart.txt" -ScriptType BAT
