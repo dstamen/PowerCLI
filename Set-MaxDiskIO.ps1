549a4536a0094f68937a69f703e95fe18fc921a9
dstamen <davidstamen@Davids-MacBook-Pro.local>
2016-01-13 10:57:50 -0500

    post

---
+++ Set-MaxDiskIO.ps1
@@ -0,0 +1,20 @@
+# PowerCLI Script for setting Disk Max IO Size
+# @davidstamen
+# http://davidstamen.com
+
+#Define Variables
+$cred = Get-Credential
+$VC = "vc.lab.local"
+$Cluster = "Production"
+
+#Connect to vCenter
+Connect-VIServer $VC -Credential $cred
+
+#Get all Hosts
+$esxHosts = Get-Cluster $Cluster | Get-VMHost | Where { $_.PowerState -eq "PoweredOn" -and $_.ConnectionState -eq "Connected" } | Sort Name
+
+#For each host set DiskMaxIOsize to 4MB
+foreach ($esx in $esxHosts) {
+  $esxcli = Get-EsxCli -VMHost $esx
+  Get-AdvancedSetting -Entity $esx -Name Disk.DiskMaxIOSize | Set-AdvancedSetting -Value 4096 -Confirm:$false
+}
