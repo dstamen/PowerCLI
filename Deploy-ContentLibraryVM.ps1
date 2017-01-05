523633790b3f634c8da67ea8523f222a61cbcf71
David Stamen <dstamen@gmail.com>
2017-01-04 20:28:42 -0500

    post

--- Deploy-ContentLibraryVM.ps1
+++ Deploy-ContentLibraryVM.ps1
@@ -1,8 +1,13 @@
-$TemplateName = "2012R2"
-$VMName = "TEST01"
-$Datastore = "Datastore"
-$Cluster = "Cluster"
-$custspec = "windows"
-Get-ContentLibraryItem -Name $TemplateName|New-VM -Datastore $Datastore -ResourcePool $Cluster -Name $VMName
+$TemplateName = "template-windows2016"
+$VMName = "ContentLibraryVM"
+$Datastore = Get-DatastoreCluster "DatastoreCluster"
+$Cluster = Get-Cluster "Cluster"
+$custspec = Get-OSCustomizationSpec "windows_workgroup"
+$ContentLibraryVM = Get-ContentLibraryItem -name $TemplateName |Where-Object {$_.ContentLibrary -like "*$Cluster*"}
+New-VM -Datastore $Datastore -ResourcePool $Cluster -Name $VMName -ContentLibraryItem $ContentLibraryVM
+if ($TemplateName -eq "template-windows2016") {
+    Write-Host "Changing GuestId for Windows 2016" -ForegroundColor Green
+    Set-VM -VM $VMName -GuestId windows9Server64Guest -Confirm:$false
+}
 Set-VM -VM $VMName -OSCustomizationSpec $custspec -Confirm:$false
 Start-VM $VMName -Confirm:$false
