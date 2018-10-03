c480fbb80b8c427f086029af49f4a0ad0bb99e46
David Stamen <dstamen@gmail.com>
2018-10-03 16:10:56 -0400

    update

--- UpgradeVDS.ps1
+++ UpgradeVDS.ps1
@@ -8,13 +8,13 @@ If ($Cluster.DrsEnabled -like "True") {
     $ClusterDRSLevel = $Cluster.DrsAutomationLevel
     Write-Host "DRS Cluster is currently set to $ClusterDRSLevel. Will change back when complete." -ForegroundColor "Green"
     Get-Cluster $Cluster | Set-Cluster -DrsAutomationLevel "PartiallyAutomated" -Confirm:$false
-    Get-VDSwitch -Name $VDSwitch | Export-VDSwitch -Description "My Backup" -Destination "/Users/dstamen/Downloads/VDSBackup-$VDswitch-$((Get-Date).ToString(‘yyyy-MM-dd-hh-mm’)).zip"
+    Get-VDSwitch -Name $VDSwitch | Export-VDSwitch -Description "My Backup" -Destination "/PathToBackup/VDSBackup-$VDswitch-$((Get-Date).ToString(‘yyyy-MM-dd-hh-mm’)).zip"
     Get-VDSwitch -Name $VDSwitch | Set-VDSwitch -Version $VDVersion
     Write-Host "Upgrade is complete. Setting Cluster to $ClusterDRSLevel." -ForegroundColor "Green"
     Get-Cluster $Cluster | Set-Cluster -DrsAutomationLevel $ClusterDRSLevel -Confirm:$false
 }
 ElseIf ($Cluster.DrsEnabled -like "False") {
     Write-Host "DRS is Disabled, No additional action needed." -ForegroundColor "Green"
-    Get-VDSwitch -Name $VDSwitch | Export-VDSwitch -Description "My Backup" -Destination "/Users/dstamen/Downloads/VDSBackup-$VDswitch-$((Get-Date).ToString(‘yyyy-MM-dd-hh-mm’)).zip"
+    Get-VDSwitch -Name $VDSwitch | Export-VDSwitch -Description "My Backup" -Destination "/PathToBackup/VDSBackup-$VDswitch-$((Get-Date).ToString(‘yyyy-MM-dd-hh-mm’)).zip"
     Get-VDSwitch -Name $VDSwitch | Set-VDSwitch -Version $VDVersion
 }
