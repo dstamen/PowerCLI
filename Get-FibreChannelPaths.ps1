5aed7f488b2fba4b7c34734bdb3a8ed26ae8edd3
David Stamen <dstamen@gmail.com>
2020-05-29 13:16:53 -0400

    update

---
+++ Get-FibreChannelPaths.ps1
@@ -0,0 +1,16 @@
+$VMHosts = Get-VMHost  | ? { $_.ConnectionState -eq "Connected" } | Sort-Object -Property Name
+$results= @()
+
+foreach ($VMHost in $VMHosts) {
+[ARRAY]$HBAs = $VMHost | Get-VMHostHba -Type "FibreChannel"
+
+    foreach ($HBA in $HBAs) {
+    $pathState = $HBA | Get-ScsiLun | Get-ScsiLunPath | Group-Object -Property state
+    $pathStateActive = $pathState | ? { $_.Name -eq "Active"}
+    $pathStateDead = $pathState | ? { $_.Name -eq "Dead"}
+    $pathStateStandby = $pathState | ? { $_.Name -eq "Standby"}
+    $results += "{0},{1},{2},{3},{4},{5}" -f $VMHost.Name, $HBA.Device, $VMHost.Parent, [INT]$pathStateActive.Count, [INT]$pathStateDead.Count, [INT]$pathStateStandby.Count
+    }
+
+}
+ConvertFrom-Csv -Header "VMHost","HBA","Cluster","Active","Dead","Standby" -InputObject $results | Ft -AutoSize
